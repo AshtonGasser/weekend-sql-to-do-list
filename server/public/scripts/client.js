2255b350626fad9e4301806f118db6c93a492bb4
Ashton Gasser <ashtongasser@gmail.com>
2021-05-30 15:03:12 -0500

    delete created, check box bs solved!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,19 +13,27 @@ setupClickListeners()
 
     //$('#viewToDo').on('click', '.transfer', handleTransferClick)
   }); // end doc ready
-
+    //console.log( 'in addButton on click' );
+    // input validation -- could make required field background to red via jquery toggle class
+    // if ($('#taskIn').val() == '' || $('#createdIn').val() == '' || 
+    // $('#importanceIn').val() == '' ) {
+    //   swal("Please fill in all required information");
+      //return; 
 
 function setupClickListeners(){
 console.log('shhh! we are listening for clicks.');
 $("#viewToDo").on('click', '.deleteBtn', deleteTodoHandler) 
 $(".dropdown-toggle" ).dropdown ()  
 $( '#addButton' ).on( 'click', saveTodoList)
-    //console.log( 'in addButton on click' );
-    // input validation -- could make required field background to red via jquery toggle class
-    // if ($('#taskIn').val() == '' || $('#createdIn').val() == '' || 
-    // $('#importanceIn').val() == '' ) {
-    //   alert("Please fill in all required information");
-    //   return; 
+
+   
+$("#completeIn").on('change', function() {
+  if ($(this).is(':checked')) {
+        $(this).attr('value', 'true');
+  } else {
+    $(this).attr('value', 'false');
+  }
+});
 
 }
 
@@ -44,16 +52,23 @@ $.ajax({
 } // end getTodo
 
 function renderTodo(todo){
-    //$('#viewTodo').empty();
+    $('#viewToDo').empty();
     console.log(todo);
     console.log(todo.length);
     for(let i = 0; i < todo.length; i++) {
         let notes = todo[i].notes ? todo[i].notes : "";
+        // ⬇ checking true or false value for complete, then dynamically setting checkbox for complete.
+        let completed;
+        if (todo[i].complete === true) {
+             completed = `<td class="bg-success"><input type="checkbox" id="completed${todo[i].id}" value="${todo[i].complete}" checked></input></td>`
+        } else {
+             completed = `<td><input type="checkbox" id="completed${todo[i].id}" value="${todo[i].complete}"></input></td>`
+        }
         let todoHTML = `
-        <tr class="${todo[i].importance}">
+        <tr>
           <td>${todo[i].task}</td>
           <td>${todo[i].created}</td>
-          <td><input type="checkbox" value="${todo[i].complete}"></input></td>
+          ${completed}
           <td>${todo[i].dateCompleted}</td>
           <td><input type="text" id="todoNotes${todo[i].id}" value="${notes}"></td>
           <td>${todo[i].importance_rank}</td>
@@ -69,10 +84,11 @@ function renderTodo(todo){
 
 function saveTodoList(){
     // ajax call to server to get todo
+    let completed = $('#completeIn').val() ? $('#completeIn').val() : false;
     let newTodo = {
         task: $('#taskIn').val(),
         created: $('#createdIn').val(),
-        complete: $('#completeIn').val(),
+        complete: completed,
        dateCompleted: $('#dateCompletedIn').val(),
         notes: $('#notesIn').val(),
         importance_rank: $('#importanceIn').val()
@@ -96,5 +112,21 @@ function saveTodoList(){
 }
   
   function deleteTodoHandler(){
-      console.log('Begon!')
+    deleteTodo($(this).data("id")) 
+    console.log('Begon!',$(this).data("id"))
   }
+
+  function deleteTodo(todoId){
+      console.log(todoId);
+      $.ajax({
+          method: 'DELETE',
+          url: `/todo/${todoId}`
+      }).then(response =>{
+          console.log(`deleted todo with id of ${todoId}`);
+          getToDoList();
+          getToDoList()
+          swal("no more todo list");
+         }).catch(err => {
+        alert('there was a problem deleting that todo, try again', err);
+         })
+        }
