55c58ff257d93743590adeb0e6f5f66e531c18eb
Ashton Gasser <ashtongasser@gmail.com>
2021-05-30 00:54:22 -0500

    bugs fixed post works, more styling need

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,25 +19,13 @@ function setupClickListeners(){
 console.log('shhh! we are listening for clicks.');
 $("#viewToDo").on('click', '.deleteBtn', deleteTodoHandler) 
 $(".dropdown-toggle" ).dropdown ()  
-$( '#addButton' ).on( 'click'),
-    console.log( 'in addButton on click' );
+$( '#addButton' ).on( 'click', saveTodoList)
+    //console.log( 'in addButton on click' );
     // input validation -- could make required field background to red via jquery toggle class
     // if ($('#taskIn').val() == '' || $('#createdIn').val() == '' || 
     // $('#importanceIn').val() == '' ) {
     //   alert("Please fill in all required information");
-    //   return;
-    
-    let TodoListToSend = {
-        task: $('taskIn').val(),
-        created: $('#createdIn').val(),
-        complete: $('#completeIn').val(),
-       dateCompleted: $('#dateCompletedIn').val(),
-        notes: $('#notesIn').val(),
-        importance_rank: $('#importanceIn').val()
-  };    
-      // call savetodolist with the new object
-      saveTodoList(TodoListToSend);
-
+    //   return; 
 
 }
 
@@ -56,37 +44,41 @@ $.ajax({
 } // end getTodo
 
 function renderTodo(todo){
-    $('#viewTodo').empty();
-
+    //$('#viewTodo').empty();
+    console.log(todo);
+    console.log(todo.length);
     for(let i = 0; i < todo.length; i++) {
-        let todo = todo[i];
-        console.log('in render todo', todo);
-
+        let notes = todo[i].notes ? todo[i].notes : "";
+        let todoHTML = `
+        <tr class="${todo[i].importance}">
+          <td>${todo[i].task}</td>
+          <td>${todo[i].created}</td>
+          <td><input type="checkbox" value="${todo[i].complete}"></input></td>
+          <td>${todo[i].dateCompleted}</td>
+          <td><input type="text" id="todoNotes${todo[i].id}" value="${notes}"></td>
+          <td>${todo[i].importance_rank}</td>
+          <td><button class="deleteBtn btn btn-danger" data-id="${todo[i].id}">Delete</button></td>
+        </tr>`
         //for each Todo list, append a nw row to table
-        $('#viewTodo').append(`
-    <tr>
-      <td>${todo.task}</td>
-      <td>${todo.created}</td>
-      <td>${todo.complete}</td>
-      <td>${todo.dateCompleted}</td>
-      <td>${todo.notes}</td>
-      <td>${todo.importance_rank}</td>
-      <td><button class="deleteBtn btn btn-danger" data-id="${todo.id}">Delete</button></td>
-    </tr>
-        
-        `)
-
-}
-
+        $('#viewToDo').append(todoHTML)
+    }
 }
 
 
 
 
-function saveTodoList(newTodo){
-    console.log( ' saveTodoList', newTodo );
-    // ajax call to server to get koalas
-   
+function saveTodoList(){
+    // ajax call to server to get todo
+    let newTodo = {
+        task: $('#taskIn').val(),
+        created: $('#createdIn').val(),
+        complete: $('#completeIn').val(),
+       dateCompleted: $('#dateCompletedIn').val(),
+        notes: $('#notesIn').val(),
+        importance_rank: $('#importanceIn').val()
+
+  }; 
+  console.log( ' saveTodoList', newTodo );   
     $.ajax({
       type: 'POST',
       url: '/todo',
@@ -94,7 +86,7 @@ function saveTodoList(newTodo){
   }).then( function (response) {
     $('#taskIn').val('');
     $('#createdIn').val('');
-    $('#completedIn').val('');
+    $('#completeIn').val('');
     $('#notesIn').val('');
     $('#dateCompleted').val('');
     $('#importanceIn').val('');
